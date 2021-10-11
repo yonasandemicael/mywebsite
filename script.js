1d9f8d00ca8969a1abbc1e493ce3996419c86907
yonasandemicael <73049970+yonasandemicael@users.noreply.github.com>
2021-10-10 20:42:06 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("it is working");
+let theme = localStorage.getItem("theme");
+if (theme == null) {
+  setTheme("light");
+} else {
+  setTheme(theme);
+}
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (let i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("option clicked", mode);
+    setTheme(mode);
+  });
+}
+function setTheme(mode) {
+  if (mode == "light")
+    document.getElementById("theme-style").href = "default.css";
+  if (mode == "blue") document.getElementById("theme-style").href = "blue.css";
+  if (mode == "green")
+    document.getElementById("theme-style").href = "green.css";
+  if (mode == "purple")
+    document.getElementById("theme-style").href = "purple.css";
+  // pass the them to set the local storage value
+  localStorage.setItem("theme", mode);
+}
